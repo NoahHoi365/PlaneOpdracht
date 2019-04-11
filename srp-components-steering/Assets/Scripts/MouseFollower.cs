f8ec5ff31bdb78575cda3e1a4b7afed903ae6c08
Noah Eelman <noah.eelman@xs4all.nl>
2019-04-11 08:54:18 +0200

    lulkoek

---
+++ srp-components-steering/Assets/Scripts/MouseFollower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollower : MonoBehaviour {
+
+    public GameObject target;
+    public delegate Vector2 planePos();
+    
+    void Update () {
+        // Als je met de muis klikt: geef dan aan SteeringVehicle door
+        // via SetTarget() waar het vliegtuig naartoe moet
+        if (Input.GetMouseButtonDown(0))
+        {
+            target.transform.position = GetMousePosition();      
+        }
+        
+    }
+
+    Vector2 GetMousePosition()
+    {
+        var mousePosition = Input.mousePosition;
+        mousePosition.z = 10;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
